cad732fe334ed18ef585501934c82dbf7d3674e0
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-03-01 23:32:58 +1300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ldap-sandbox
+
+Tests with LDAP.
+
+Docker used during tests: https://hub.docker.com/r/rroemhild/test-openldap
---
+++ src/main/java/br/eti/kinoshita/ldap_sandbox/Test.java
@@ -0,0 +1,49 @@
+package br.eti.kinoshita.ldap_sandbox;
+
+import javax.mail.internet.InternetAddress;
+
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
+import com.unboundid.ldap.sdk.migrate.ldapjdk.LDAPEntry;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        String realEmail = "hubert@planetexpress.com";
+        //String fakeEmail = asciify("))(notmyemail@anotherexample.com,");
+        String fakeEmail = "))(|(mail=notmyemail@anotherexample.com)(";
+        String email = String.format("%s%s", realEmail, fakeEmail);
+        String filter = "(&(mail=" + email + ")(objectClass=*))";
+        System.out.println(filter);
+        final String host = "localhost";
+        final int port    = 1389;
+        final String base = "ou=people,dc=planetexpress,dc=com"; // "dc=planetexpress,dc=com";
+        final String user = "cn=admin,dc=planetexpress,dc=com";
+        final String pass = "GoodNewsEveryone";
+        LDAPConnection connection = new LDAPConnection(host, port, user, pass);
+
+        SearchResult entries = connection.search(base, SearchScope.SUB, filter);
+        if (entries.getSearchEntries().size() == 0) {
+            throw new RuntimeException("No LDAP search results!");
+        }
+        for (SearchResultEntry entry : entries.getSearchEntries()) {
+            LDAPEntry ldapEntry = new LDAPEntry(entry);
+            String cn = ldapEntry.getAttribute("cn").toString();
+            System.out.println(cn);
+        }
+
+        connection.close();
+        InternetAddress.parse(email);
+        System.out.println("OK!");
+    }
+
+    private static String asciify(String origin) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < origin.length(); i++) {
+            sb.append("%" + (int) origin.charAt(i));
+        }
+        return sb.toString();
+    }
+}
